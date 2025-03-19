7b9876fee05c98f712af4ca6cdbc20ef4cc935f4
anhnmt <anhng.dev@gmail.com>
2025-03-19 22:01:55 +0700

    Optimize default setting, validate

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/pflag"
 )
 
@@ -17,13 +18,17 @@ var cfg Config
 func main() {
 	pflag.StringVar(&cfg.SeedPhrase, "s", "", "your seed phrase")
 	pflag.StringVar(&cfg.DerivationPath, "p", "m/44'/314159'/0'", "derivation path")
-	pflag.IntVar(&cfg.MaxWordMissing, "m", 10, "max word missing")
+	pflag.IntVar(&cfg.MaxWordMissing, "m", 5, "max word missing")
 	pflag.IntVar(&cfg.BatchSize, "b", 10, "batch size")
 	pflag.BoolVar(&cfg.Debug, "d", true, "debug mod")
 	pflag.Parse()
 
 	// init logger
 	initLogger(cfg.Debug)
+
+	if cfg.SeedPhrase == "" {
+		log.Fatal().Msg("Seed phrase is empty")
+	}
 
 	// recover seed phrase
 	recoverSeedPhrase(cfg)
